894960bde555f89dea25d18f0fc9fa941170ff42
Megan Knaresborough <Megan.Knaresborough@simplyhealth.co.uk>
2024-07-05 12:17:51 +0100

    navigation working

--- App.tsx
+++ App.tsx
@@ -8,9 +8,10 @@ import QuizStart from './Screens/QuizStart';
 import Quiz from './Screens/Quiz';
 
 
+const Stack = createStackNavigator();
+
+
 const App = () => {
-  //const
-  const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
@@ -19,7 +20,7 @@ const App = () => {
         <Stack.Screen name="CreateQuestions" component={CreateQuestions} />
         <Stack.Screen name="Quiz" component={Quiz} />
         <Stack.Screen name="QuizStart" component={QuizStart} />
-        {/* <Stack.Screen name="LeaderBoard" component={LeaderBoard} /> */}
+        {/* { <Stack.Screen name="LeaderBoard" component={LeaderBoard} />} */}
       </Stack.Navigator>
     </NavigationContainer>
   );
